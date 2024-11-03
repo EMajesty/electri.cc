004d77f6210cb296bfbd40547ec81731f28f83ed
EMajesty <lassi.laitinen@protonmail.com>
2024-11-03 12:27:10 +0200

    add rustup to build script

--- build.sh
+++ build.sh
@@ -3,7 +3,18 @@
 REPO_URL="https://github.com/emajesty/ssgoat.git"
 BUILD_DIR="./build"
 
+install_rust() {
+    echo "Installing Rust"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source "$HOME/.cargo/env"
+}
+
 echo "Hell world"
+
+if ! command -v cargo &> /dev/null; then
+    echo "Cargo not found"
+    install_rust
+fi
 
 cargo install --git $REPO_URL
 
